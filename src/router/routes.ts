b8aa864f7ca207327214581dc4381c5c968b5179
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-09-05 15:22:47 +0200

    feat: set up initial router
    Add the router view to the main page and add an empty front page to that
    router.

---
+++ src/router/routes.ts
@@ -0,0 +1,6 @@
+import FrontpageView from '@/pages/FrontpageView.vue'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [{ path: '/', component: FrontpageView }]
+
+export default routes
