343ea259f15ad392b5a25345f18ca39032042888
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-09-05 16:41:09 +0200

    deps: add phosphor icons
    Add Phosphor icons as a dependency as the codebase's icon library

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PhTray } from '@phosphor-icons/vue';
+
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold">Daily Mix</h1>
+  <div class="flex">
+    <PhTray :size="32" color="var(--heading)" weight="bold" />
+    <h1 class="text-3xl font-bold">Daily Mix</h1>
+  </div>
   <h2 class="metadata text-accent">Your one stop shop for all your news</h2>
   <nav>
     <RouterLink to="/">Frontpage</RouterLink>
