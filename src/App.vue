5775b6830bb4b3ae311c13f50221241cd15113bb
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-09-05 16:25:25 +0200

    chore: add tailwindcss (#2)
    * deps: added tailwindcss as a dependency
    
    Added TailwindCSS as a dependency to the project and set it up for use
    in Vue.
    
    * feat: add custom theme
    
    Add a custom theme to tailwindcss for the application

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Trakt</h1>
+  <h1 class="text-3xl font-bold">Daily Mix</h1>
+  <h2 class="metadata text-accent">Your one stop shop for all your news</h2>
   <nav>
     <RouterLink to="/">Frontpage</RouterLink>
   </nav>
